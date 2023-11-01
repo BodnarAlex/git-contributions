3657aa9893510ddf4cd5ea8c887ba18381c58b20
Bodnar Alex <allugovskova@mail.ru>
2023-11-01 23:24:18 +0200

    refactor: improve change of colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@ window.onload = function () {
     let checkbox = document.querySelector(".offLight");
     checkbox.addEventListener('change', e => {
         if(e.target.checked){
-            document.body.style.backgroundColor = '#21262d';
-            document.body.style.color = '#c9d1d9';
-            document.body.style.borderColor = '#b1bac4';
-
-            document.querySelector("header").style.backgroundColor = '#161b22';
-            document.querySelector("footer").style.backgroundColor = '#161b22';
-
+            document.documentElement.style.setProperty('--back', '#21262d');
+            document.documentElement.style.setProperty('--border', '#b1bac4');
+            document.documentElement.style.setProperty('--header', '#161b22');
+            document.documentElement.style.setProperty('--text', '#c9d1d9');
         }else{
-            document.body.style.backgroundColor = '#f0f6fc';
-
+            document.documentElement.style.setProperty('--back', '#f6f8fa');
+            document.documentElement.style.setProperty('--border', '#c9d1d9');
+            document.documentElement.style.setProperty('--header', '#c9d1d9');
+            document.documentElement.style.setProperty('--text', '#1F2328');
         }
     });
 }
