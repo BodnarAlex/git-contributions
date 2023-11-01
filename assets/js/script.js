72b20820b32ff56d7f631056a8d7b0b19dfb1519
Bodnar Alex <allugovskova@mail.ru>
2023-11-01 23:23:45 +0200

    feat: add main colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ window.onload = function () {
     checkbox.addEventListener('change', e => {
         if(e.target.checked){
             document.body.style.backgroundColor = '#21262d';
+            document.body.style.color = '#c9d1d9';
+            document.body.style.borderColor = '#b1bac4';
+
+            document.querySelector("header").style.backgroundColor = '#161b22';
+            document.querySelector("footer").style.backgroundColor = '#161b22';
 
         }else{
             document.body.style.backgroundColor = '#f0f6fc';
