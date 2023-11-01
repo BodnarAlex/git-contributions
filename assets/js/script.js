462f66541a14596e79087fb338a3a3a0477270cc
Bodnar Alex <allugovskova@mail.ru>
2023-11-01 23:06:03 +0200

    feat: add button of background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 window.onload = function () {
-  
+    let checkbox = document.querySelector(".offLight");
+    checkbox.addEventListener('change', e => {
+        if(e.target.checked){
+            document.body.style.backgroundColor = '#21262d';
+
+        }else{
+            document.body.style.backgroundColor = '#f0f6fc';
+
+        }
+    });
 }
